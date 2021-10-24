84cda3eed9538df02fcaf692daa5cbb5d4b4c6dd
ruchitha1405 <83824520+ruchitha1405@users.noreply.github.com>
2021-10-24 20:35:24 +0530

    Update Anonymous3.sql

--- Anonymous3.sql
+++ Anonymous3.sql
@@ -24,7 +24,7 @@ CREATE TABLE Tables(location varchar(20) PRIMARY KEY,Table_no int PRIMARY KEY,nu
 CREATE TABLE delivery_apps(Delivery_Id int PRIMARY KEY,Name_of_DeliveryApp VARCHAR(20));
 
 
-CREATE TABLE menu(location varchar(20) , menu varchar(20) ,CONSTRAINT PK_menu PRIMARY KEY (location,menu));
+CREATE TABLE menu(location varchar(20) , item_num varchar(20) ,CONSTRAINT PK_menu PRIMARY KEY (location,item_num));
 
 
 CREATE TABLE tablesbooked(phone_num int  FOREIGN KEY, table_num int ,CONSTRAINT PK_tablesbooked PRIMARY KEY (phone_num,table_num));
