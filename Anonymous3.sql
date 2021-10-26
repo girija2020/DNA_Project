1ed5e0350d281189a5f26b4ac31a1c36f7307b4e
girija2020 <82602432+girija2020@users.noreply.github.com>
2021-10-26 14:40:19 +0530

    Update Anonymous3.sql

--- Anonymous3.sql
+++ Anonymous3.sql
@@ -6,7 +6,7 @@ CREATE TABLE restaurant(Location_of_Restaurant varchar(20) PRIMARY KEY, Name_of_
 
 CREATE TABLE food_items(item_number int PRIMARY KEY, Descript varchar(100), rating int, price float);
 
-CREATE TABLE employee(employee_id int PRIMARY KEY, profession varchar(20) FOREIGN KEY, restaurant_location varchar(20) FOREIGN KEY, Employee_name varchar(20), Restaurant_name varchar(20), PF_amount float, Month_of_joining int, Year_of_joining int, Date_of_joining int);
+CREATE TABLE employee(employee_id int PRIMARY KEY, profession varchar(20) REFERENCES Profession.Profession, restaurant_location varchar(20) REFERENCES restaurant.Location_of_Restaurant, Employee_name varchar(20), Restaurant_name varchar(20), PF_amount float, Month_of_joining int, Year_of_joining int, Date_of_joining int);
 
 CREATE TABLE Ordered_items(Order_id int FOREIGN KEY, Ordered_item_number int FOREIGN KEY, Number_of_Items int);
 
