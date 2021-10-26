22603be3aa2affeaca115ccd0b04551ea9f22639
ruchitha1405 <83824520+ruchitha1405@users.noreply.github.com>
2021-10-26 15:04:09 +0530

    Add files via upload

---
+++ restaurant_chain.py
@@ -0,0 +1,67 @@
+import subprocess as sp
+import pymysql
+import pymysql.cursors
+
+def dispatch(ch):
+    """
+    Function that maps helper functions to option entered
+    """
+
+    if(ch==1): 
+        Insertion()
+    elif(ch==2):
+        Updation()
+    elif(ch==3):
+        Deletion()
+    elif(ch==4):
+        Report()
+    else:
+        print("Error: Invalid Option")
+
+# Global
+while(1):
+    tmp = sp.call('clear',shell=True)
+    username = input("username: ")
+    password = input("password: ")
+
+    try:
+        con = pymysql.connect(host='localhost',
+                user=username,
+                password=password,
+                db='RESTAURANT_CHAIN',
+                cursorclass=pymysql.cursors.DictCursor)
+        tmp = sp.call('clear',shell=True)
+
+        if(con.open):
+            print("Connected")
+        else:
+            print("Failed to connect")
+        tmp = input("Enter any key to CONTINUE>")
+
+        with con:
+            cur = con.cursor()
+            while(1):
+                tmp = sp.call('clear',shell=True)
+                print("")
+                print("======================Functions of RESTAURANT_CHAIN Database======================")
+                print("")
+                print("1. Insertion")
+                print("2. Updation")
+                print("3. Delete")
+                print("4. Report")
+                print("5. Logout")
+                print("")
+                ch = int(input("Enter choice> "))
+                tmp = sp.call('clear',shell=True)
+                if ch==5:
+                    break
+                else:
+                    dispatch(ch)
+                    tmp = input("Enter any key to CONTINUE>")
+
+
+    except:
+        tmp = sp.call('clear',shell=True)
+        print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
+        tmp = input("Enter any key to CONTINUE>")
+    
