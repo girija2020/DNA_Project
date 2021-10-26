1d733a55383af653cac59d357bec9fa5152f9c98
ruchitha1405 <83824520+ruchitha1405@users.noreply.github.com>
2021-10-26 20:05:45 +0530

    Update restaurant_chain.py

--- restaurant_chain.py
+++ restaurant_chain.py
@@ -2,6 +2,153 @@ import subprocess as sp
 import pymysql
 import pymysql.cursors
 
+import subprocess as sp
+import pymysql
+import pymysql.cursors
+
+def Insertion():
+    
+    print("1. Insertion of a New employee and his educational qualification")
+    print("3. Insertion of a New customer")
+    #print("3. Insertion of a New food item and its ingredients")#
+    #print("4. Insertion of  a New table")
+    #print("4. Insertion of a New restaurant")#
+    print("2. Inserting a new profession")
+    #print("6. Inserting a new order")#
+    #print("7. Inserting a new fooditem in the menu of a restaurant")
+    print("4. Exit")
+    
+    val = int(input("Enter your choice: "))
+
+    if val == 4:
+        return
+    
+    elif val == 1:
+        try:
+            print("")
+            print("Enter the employee's Details: ")
+            player={}
+            player["employee_id"]=int(input("employee_id: "))
+            #name= input("Name (Fname Lname): ").split(' ')
+            #player["Fname"] = name[0]
+            #player["Lname"] = name[1]
+            
+            player["profession"] = (input("profession: "))
+            player["restaurant_location"] = (input("restaurant_location: "))
+            player["Employee_name"] = (input("Employee_name: "))
+            player["Restaurant_name"] = (input("Restaurant_name: "))
+            player["PF_amount"] = float(input("PF_amount: "))
+            player["Month_of_joining"] = int(input("Month_of_joining: "))
+            player["Year_of_joining"] = int(input("Year_of_joining: "))
+            player["Date_of_joining"] = int(input("Date_of_joining: "))
+            
+            
+            query= "INSERT INTO employee VALUES('%d', '%s', '%s', '%s', '%s', '%f', '%d', '%d','%d')" %(player["employee_id"],player["profession"],player["restaurant_location"], 
+            player["Employee_name"],player["Restaurant_name"],player["PF_amount"],player["Month_of_joining"],player["Year_of_joining"],player["Date_of_joining"])
+            cur.execute(query)
+            # print(query)
+
+            
+
+            print("")
+            print("==============================================INSERTED==============================================")
+            print("")
+
+            print("EMPLOYEE TABLE")
+            print("")
+            query="SELECT * FROM employee"
+            cur.execute(query)
+            result=cur.fetchall()
+            print(result)
+            print("")
+            
+      
+      
+        except Exception as e:
+            con.rollback()
+            print("Failed to Insert Into the Database")
+            print(">>>>>>>>>>>>>",e)
+        
+        return
+
+    elif val == 2:
+        try:
+
+            team={}
+            print("")
+            print("Enter New Profession's Details: ")
+            team["Profession"]=(input("Profession: "))
+            team["Salary"]=int(input("Salary: "))
+            team["Employee_Working_Hours"]=int(input("Employee_Working_Hours: "))
+            team["PF_Percentage"]=int(input("PF_Percentage: "))
+
+            query= "INSERT INTO Profession VALUES('%s', '%d','%d','%d')"%(team["Profession"], team["Salary"],team["Employee_Working_Hours"],team["PF_Percentage"])
+            cur.execute(query)
+            con.commit()
+            # print(query)
+
+            
+
+            print("")
+            print("==============================================INSERTED==============================================")
+            print("")
+
+            print("PROFESSION TABLE")
+            print("")
+            query="SELECT * FROM Profession"
+            cur.execute(query)
+            result=cur.fetchall()
+            print(result)
+            print("")
+
+            
+
+
+        except Exception as e:
+            con.rollback()
+            print("Failed to Insert Into the Database")
+            print(">>>>>>>>>>>>>",e)
+
+    
+
+    elif val == 3:
+        try:
+            team={}
+            print("")
+            print("Enter New customer's Details: ")
+            team["Phone_number"]=int(input("Phone_number: "))
+            team["user_name"]=(input("user_name: "))
+            team["Email"]=(input("Email: "))
+            team["Amount_Recieved"]=True
+            team["Name"]=(input("Name: "))
+            team["Order_number"]=pymysql.NULL
+
+            query= "INSERT INTO Customer VALUES('%d', '%s','%s', 'True','%s','NULL')"%(team["Phone_number"], team["user_name"],team["Email"],team["Name"])
+            cur.execute(query)
+            con.commit()
+            # print(query)
+
+            
+
+            print("")
+            print("==============================================INSERTED==============================================")
+            print("")
+
+            print("CUSTOMER TABLE")
+            print("")
+            query="SELECT * FROM Customer"
+            cur.execute(query)
+            result=cur.fetchall()
+            print(result)
+            print("")
+
+            
+
+
+        except Exception as e:
+            con.rollback()
+            print("Failed to Insert Into the Database")
+            print(">>>>>>>>>>>>>",e)
 def dispatch(ch):
     """
     Function that maps helper functions to option entered
